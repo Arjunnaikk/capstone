08fec83f672e7fb53fc094b13b63517462465c90
Parthesh28 <partheshpurohit28@gmail.com>
2026-02-17 16:41:44 +0530

    create_project done

--- backend/programs/capstone/src/instructions/create_project.rs
+++ backend/programs/capstone/src/instructions/create_project.rs
@@ -2,29 +2,8 @@ use anchor_lang::prelude::*;
 
 use crate::{errors::Error, state::project::*};
 
-pub fn initialize_project(
-    ctx: Context<InitializeProject>,
-    project_id: String,
-    target_amount: u64,
-    deadline: i64,
-) -> Result<()> {
-    let project = &mut ctx.accounts.project;
-
-    require!(target_amount > 0, Error::ZeroAmount);
-
-    project.set_inner(Project {
-        project_authority: ctx.accounts.project_authority.key(),
-        target_amount: target_amount,
-        collected_amount: 0,
-        project_state: ProjectState::Funding,
-        bump: ctx.bumps.project,
-    });
-
-    Ok(())
-}
-
 #[derive(Accounts)]
-#[instruction(project_id: String)]
+#[instruction(project_name: String)]
 pub struct InitializeProject<'info> {
     #[account(mut)]
     pub project_authority: Signer<'info>,
@@ -32,11 +11,36 @@ pub struct InitializeProject<'info> {
     #[account(
         init,
         space = Project::DISCRIMINATOR.len() +  Project::INIT_SPACE,
-        seeds= [PROJECT_SEED, project_id.as_bytes(), project_authority.key().as_ref()],
+        seeds= [PROJECT_SEED, project_name.as_bytes(), project_authority.key().as_ref()],
         payer = project_authority,
         bump
     )]
     pub project: Box<Account<'info, Project>>,
 
     pub system_program: Program<'info, System>,
+}
+
+impl<'info> InitializeProject<'info> {
+    pub fn initialize_project(
+        ctx: Context<InitializeProject>,
+        project_name: String,
+        target_amount: u64,
+        deadline: i64,
+    ) -> Result<()> {
+        let project = &mut ctx.accounts.project;
+
+        require!(target_amount > 0, Error::ZeroAmount);
+
+        project.set_inner(Project {
+            project_authority: ctx.accounts.project_authority.key(),
+            project_name,
+            target_amount: target_amount,
+            collected_amount: 0,
+            project_state: ProjectState::Funding,
+            project_deadline: deadline,
+            bump: ctx.bumps.project,
+        });
+
+        Ok(())
+    }
 }
--- backend/programs/capstone/src/state/project.rs
+++ backend/programs/capstone/src/state/project.rs
@@ -6,10 +6,13 @@ pub const PROJECT_SEED: &[u8] = b"PROJECT";
 #[derive(InitSpace)]
 pub struct Project {
     pub project_authority: Pubkey,
+    #[max_len(50)]
+    pub project_name: String, 
     #[max_len(32)]
     pub target_amount: u64,
     pub collected_amount: u64,
     pub project_state: ProjectState,
+    pub project_deadline: i64, 
     pub bump: u8,
 }
 
