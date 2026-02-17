204e332b4455c668962d9391b701baa614c29619
Parthesh28 <partheshpurohit28@gmail.com>
2026-02-17 15:55:31 +0530

    initialized project

---
+++ programs/capstone/src/lib.rs
@@ -0,0 +1,16 @@
+use anchor_lang::prelude::*;
+
+declare_id!("3fk3PSD454iVm83yGsWtMo3xrwwV8MAUtSMhYPvimzTo");
+
+#[program]
+pub mod capstone {
+    use super::*;
+
+    pub fn initialize(ctx: Context<Initialize>) -> Result<()> {
+        msg!("Greetings from: {:?}", ctx.program_id);
+        Ok(())
+    }
+}
+
+#[derive(Accounts)]
+pub struct Initialize {}
