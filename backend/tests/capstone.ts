6d8b12d063c35c2020021937b0ed54fb8d98e8f6
Parthesh28 <partheshpurohit28@gmail.com>
2026-02-28 22:36:29 +0530

    fixed create project test

--- backend/tests/capstone.ts
+++ backend/tests/capstone.ts
@@ -6,12 +6,9 @@ import { assert } from "chai";
 import fs from "fs";
 import crypto from 'crypto'
 
+describe("capstone", () => {
 
-
-
- describe("capstone", () => {
-
-  const provider = anchor.AnchorProvider.env(); 
+  const provider = anchor.AnchorProvider.env();
   anchor.setProvider(provider);
 
   async function fundWallet(
@@ -30,20 +27,20 @@ import crypto from 'crypto'
     await provider.sendAndConfirm(tx, [from]);
   }
 
-   const program = anchor.workspace.capstone as Program<Capstone>;
-   
-   function loadWallet(path: string): Keypair {
-     const secret = JSON.parse(fs.readFileSync(path, "utf-8"));
-     return Keypair.fromSecretKey(Uint8Array.from(secret));
-   }
+  const program = anchor.workspace.capstone as Program<Capstone>;
 
-   const admin = loadWallet("./wallets/admin.json");
-   const user = loadWallet("./wallets/user.json");
-   const contributor1 = loadWallet("./wallets/contributor1.json");
-   const contributor2 = loadWallet("./wallets/contributor2.json");
-   const contributor3 = loadWallet("./wallets/contributor3.json");
-   const contributor4 = loadWallet("./wallets/contributor4.json");
-   const contributor5 = loadWallet("./wallets/contributor5.json");
+  function loadWallet(path: string): Keypair {
+    const secret = JSON.parse(fs.readFileSync(path, "utf-8"));
+    return Keypair.fromSecretKey(Uint8Array.from(secret));
+  }
+
+  const admin = loadWallet("./wallets/admin.json");
+  const user = loadWallet("./wallets/user.json");
+  const contributor1 = loadWallet("./wallets/contributor1.json");
+  const contributor2 = loadWallet("./wallets/contributor2.json");
+  const contributor3 = loadWallet("./wallets/contributor3.json");
+  const contributor4 = loadWallet("./wallets/contributor4.json");
+  const contributor5 = loadWallet("./wallets/contributor5.json");
 
   async function loadMoney() {
     await fundWallet(provider.wallet.payer, admin.publicKey, 10);
@@ -54,7 +51,7 @@ import crypto from 'crypto'
     await fundWallet(provider.wallet.payer, contributor4.publicKey, 10);
     await fundWallet(provider.wallet.payer, contributor5.publicKey, 10);
   }
-   
+
   const VAULT_SEED = "VAULT";
   const PROJECT_SEED = "PROJECT";
   const CONTRIBUTION_SEED = "CONTRIBUTION";
@@ -62,8 +59,8 @@ import crypto from 'crypto'
   const USER_SEED = "USER";
   const VOTE_SEED = "VOTE";
 
-   it("Initializes the vault", async () => {
-     await loadMoney();
+  it("Initializes the vault", async () => {
+    await loadMoney();
 
     // derive Vault PDA
     const [vaultPda, vaultBump] = PublicKey.findProgramAddressSync(
@@ -93,7 +90,7 @@ import crypto from 'crypto'
 
   it("Initializes a separate user account", async () => {
 
-    await loadMoney(); 
+    await loadMoney();
 
     // derive User PDA
     const [userPda, bump] = PublicKey.findProgramAddressSync(
@@ -110,10 +107,10 @@ import crypto from 'crypto'
         userAccount: userPda,
         systemProgram: SystemProgram.programId,
       })
-      .signers([user]) 
+      .signers([user])
       .rpc();
 
-    const userAccount = await program.account.user.fetch(userPda);``
+    const userAccount = await program.account.user.fetch(userPda); ``
 
     assert.strictEqual(userAccount.donatedAmount.toNumber(), 0, "donated amount is not correct");
     assert.strictEqual(userAccount.totalVotes.toNumber(), 0, "total votes are not correct");
@@ -130,55 +127,76 @@ import crypto from 'crypto'
 
   it("Creates a project successfully", async () => {
 
-  const projectName = "MyProject1";
-  const milestoneCount = 3;
-  const targetAmount = new anchor.BN(5 * anchor.web3.LAMPORTS_PER_SOL);
-  const deadline = new anchor.BN(Math.floor(Date.now() / 1000) + 7 * 24 * 60 * 60);
+    const projectName = "MyTestProject1";
+    const milestoneCount = 3;
+    const targetAmount = new anchor.BN(5 * anchor.web3.LAMPORTS_PER_SOL);
 
-  const [projectPda, bump] = PublicKey.findProgramAddressSync(
-    [
-      Buffer.from(PROJECT_SEED),
-      Buffer.from(projectName),
-      user.publicKey.toBuffer(),
-    ],
-    program.programId
-  );
+    const deadline = new anchor.BN(
+      Math.floor(Date.now() / 1000) + 7 * 24 * 60 * 60
+    );
 
-  const [userPda] = PublicKey.findProgramAddressSync(
-    [Buffer.from(USER_SEED), user.publicKey.toBuffer()],
-    program.programId
-  );
+    const [userPda] = PublicKey.findProgramAddressSync(
+      [Buffer.from(USER_SEED), user.publicKey.toBuffer()],
+      program.programId
+    );
 
-  const beforeUser = await program.account.user.fetch(userPda);
+    const [projectPda, projectBump] =
+      PublicKey.findProgramAddressSync(
+        [
+          Buffer.from(PROJECT_SEED),
+          Buffer.from(projectName),
+          user.publicKey.toBuffer(),
+        ],
+        program.programId
+      );
 
-  await program.methods
-    .createProject(projectName, milestoneCount, targetAmount, deadline)
-    .accountsStrict({
-      projectAuthority: user.publicKey,
-      project: projectPda,
-      user: userPda,
-      systemProgram: SystemProgram.programId,
-    })
-    .signers([user])
-    .rpc();
+    const beforeUser = await program.account.user.fetch(userPda);
 
-  const projectAccount = await program.account.project.fetch(projectPda);
-  const afterUser = await program.account.user.fetch(userPda);
+    await program.methods
+      .createProject({
+        projectName,
+        milestoneCount,
+        targetAmount,
+        deadline,
+      })
+      .accountsStrict({
+        projectAuthority: user.publicKey,
+        project: projectPda,
+        user: userPda,
+        systemProgram: SystemProgram.programId,
+      })
+      .signers([user])
+      .rpc();
 
-  assert.strictEqual(projectAccount.projectAuthority.toString(), user.publicKey.toString());
-  assert.strictEqual(projectAccount.projectName, projectName);
-  assert.strictEqual(projectAccount.targetAmount.toString(), targetAmount.toString());
-  assert.strictEqual(projectAccount.collectedAmount.toNumber(), 0);
-  assert.strictEqual(projectAccount.withdrawnAmount.toNumber(), 0);
-  assert.strictEqual(projectAccount.milestoneCount, milestoneCount);
-  assert.strictEqual(projectAccount.milestonesCompleted, 0);
-  assert.strictEqual(projectAccount.funderCount, 0);
+    const projectAccount = await program.account.project.fetch(projectPda);
+    const afterUser = await program.account.user.fetch(userPda);
 
-  // User updated
-  assert.strictEqual(
-    afterUser.projectsPosted.toNumber(),
-    beforeUser.projectsPosted.toNumber() + 1
-  );
-});
+    assert.strictEqual(
+      projectAccount.projectAuthority.toString(),
+      user.publicKey.toString()
+    );
+
+    assert.strictEqual(projectAccount.projectName, projectName);
+    assert.strictEqual(
+      projectAccount.targetAmount.toString(),
+      targetAmount.toString()
+    );
+
+    assert.strictEqual(projectAccount.collectedAmount.toNumber(), 0);
+    assert.strictEqual(projectAccount.withdrawnAmount.toNumber(), 0);
+    assert.strictEqual(projectAccount.projectState.funding !== undefined, true);
+
+    assert.strictEqual(projectAccount.milestoneCount, milestoneCount);
+    assert.strictEqual(projectAccount.milestonesCompleted, 0);
+    assert.strictEqual(projectAccount.funderCount, 0);
+
+    assert.strictEqual(projectAccount.bump, projectBump);
+
+    assert.strictEqual(
+      afterUser.projectsPosted.toNumber(),
+      beforeUser.projectsPosted.toNumber() + 1
+    );
+
+  });
 
 });
